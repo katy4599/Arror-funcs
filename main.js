0533bbc0509812d675b6d6a0c84d7cd06b451749
katy millard <katy.millard@gmail.com>
2022-02-09 09:54:33 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const names = [
+    'Lisa',
+    'DShane',
+    'Katy',
+    'Parish',
+    'Abbi',
+]
+
+function main() {
+    const sorted = names.sort((a, b) => {
+    
+    if(a < b) { 
+        return -1; 
+    } else if(a > b) { 
+        return 1; 
+    } else {
+        return 0;
+    }
+    });
+    
+console.log(sorted);
+}
+
+main();
